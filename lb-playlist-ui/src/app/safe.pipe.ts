e324e144e8bb5f208b4c1dcd9ddd5c11ccbbcddf
thatkookooguy <neilkalman@gmail.com>
2023-10-18 09:43:29 +0300

    create safe pipe for sanitizing urls

---
+++ lb-playlist-ui/src/app/safe.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safe'
+})
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url: any) {
+    return this.sanitizer.sanitize(
+      SecurityContext.RESOURCE_URL,
+      url
+    );
+  }
+}
