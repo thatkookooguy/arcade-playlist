ac799b25a26e111c0205879254193179e7b28099
thatkookooguy <neilkalman@gmail.com>
2023-11-12 11:38:58 +0200

    add storybook for components

---
+++ docs/storybook/@kibibit/shared/sb-addons/kibibit-shared-storybook-9/manager-bundle.js
@@ -0,0 +1,3 @@
+try{
+var _=__STORYBOOKADDONS__,{addons:o,types:p,mockChannel:c}=__STORYBOOKADDONS__;var g=__STORYBOOKTHEMING__,{CacheProvider:k,ClassNames:S,Global:C,ThemeProvider:u,background:x,color:y,convert:B,create:a,createCache:G,createGlobal:N,createReset:R,css:v,darken:D,ensure:K,ignoreSsrWarning:Y,isPropValid:I,jsx:P,keyframes:w,lighten:A,styled:E,themes:H,typography:M,useTheme:V,withTheme:j}=__STORYBOOKTHEMING__;var i=a({base:"dark",brandTitle:"kibibit",brandUrl:"https://kibibit.io",brandImage:"https://kibibit.io/kibibit-assets/SVG/long-white.svg",brandTarget:"_self",fontBase:"Comfortaa, sans-serif",fontCode:"monospace"});o.setConfig({theme:i});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
